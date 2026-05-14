d4b36d397f0c7e6cc0b47d7ed5a973fdd7eaa5b5
Eduardo Mateus <eduardo.mateus@estudante.ufjf.br>
2026-05-13 21:59:41 -0300

    atualiza nomes de animais com star ratings

--- Aula08/src/StarRating.js
+++ Aula08/src/StarRating.js
@@ -2,6 +2,11 @@ export default class StarRating extends HTMLElement {
     constructor() {
         super()
         console.log("Star Rating created!");
+        let stars = 3;
+        this.textContent = "";
+        for (let i = 0; i < stars; i++) {
+            this.textContent += "★";
+        }
     }
 }
 
